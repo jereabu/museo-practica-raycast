3a39683aef7898af0802d88a9027cf99e2c7087f
Jeremias_Abulafia <jeremiasabulafia@gmail.com>
2022-10-10 20:27:14 -0300

    started ray

---
+++ Assets/scrpit/raycastinteractuar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class raycastinteractuar : MonoBehaviour
+{
+    public GameObject player;
+    RaycastHit hit;
+    public float distancia;
+    public Text interactuar;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Ray Rayo = new Ray(player.transform.position, Vector3.forward);
+        Debug.DrawRay(transform.position, Vector3.forward * distancia);
+        if (Physics.Raycast(Rayo, out hit, distancia)){
+            if (hit.collider.tag == "atraccion")
+            {
+                interactuar.enabled = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    interactuar.enabled = false;
+                }
+            }
+        }
+    }
+}
